59a301d91122ca5d59eb828ba0a63ee5568707fe
skippylol <xross1b1@gmail.com>
2017-11-21 21:42:27 +0100

    Init

---
+++ ToDoEvents/ToDoEvents/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoEvents.MvcApplication" Language="C#" %>
---
+++ ToDoEvents/ToDoEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoEvents.Startup))]
+namespace ToDoEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
